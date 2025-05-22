d9f2331e75911f8fb08dd8a23de7ee54839be114
Guser06 <gusegus@hotmail.com>
2025-05-21 22:28:33 -0600

    GJ, Transposicion, ingreso datos, ejemplos

--- GaussJordan.py
+++ GaussJordan.py
@@ -1,5 +1,31 @@
-m = [[1, 2], [-1, 2], [3, 1], [4, 2]]
-v = [0, 0, 0, 0]
+##Funciones
+
+def ingreso_datos(tipo):
+    ##Caso ingresar un vector
+    if tipo == 'v':
+        print("Ingrese el numero de variables del vector")
+        n = int(input())
+        v = []
+        for i in range(n):
+            print(f"Ingrese el coeficiente {i} del vector")
+            vec = float(input())
+            v.append(vec)
+        return v
+    ##Caso ingresar una matriz
+    if tipo == 'm':
+        print("Ingrese el numero de filas de la matriz")
+        f = int(input())
+        print("Ingrese el numero de columnas de la matriz")
+        c = int(input())
+        m = []
+        for i in range(f):
+            n = []
+            for j in range(c):
+                print(f"Ingrese el valor {j+1} de la fila {i+1}")
+                val = float(input())
+                n.append(val)
+            m.append(n)
+        return m
 
 def transposicion(matriz):
     n = []
@@ -9,11 +35,6 @@ def transposicion(matriz):
             col.append(matriz[f][c])
         n.append(col) 
     return n
-
-t = transposicion(m)
-print(t)
-print()
-
 
 ##Dependiendo de para que se use la función, se agregarán previamente unos, ceros o lo que convenga
 ##Al final de cada fila de la matriz
@@ -46,6 +67,16 @@ def GJ(matriz, valores):
                     valores[f] -= v_mult
 
     return(matriz, valores) 
+
+#Ejemplos
+m = ingreso_datos('m')
+print(m)
+
+t = transposicion(m)
+print(t)
+
+v = ingreso_datos('v')
+print(v)
 
 g = GJ(m, v)
 ##Bucle para imprimir
